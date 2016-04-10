115d796b8b842b962ccf1c1c2e2ca18fff9a95e7
Stanislav Abramov <abramov.center@yandex.ru>
2016-04-10 12:15:09 +0000

    library.h edited online with Bitbucket

--- library.h
+++ library.h
@@ -1,6 +1,12 @@
 #ifndef LIBRARY_H
 #include <stdio.h>
 
-void* reader(FILE* file);
+typedef void* graph_user;
+
+graph_user reader(FILE* file);
+int existence(graph_user temp_graf, int a, int b);
+void* delete_graph(graph_user graph);
+int count_edges(graph_user temp_graf);
+int count_node(graph_user temp_graf);
 
 #endif
