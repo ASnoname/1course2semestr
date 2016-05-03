da11cd61b467462e363348f85bf16f3f43a9d458
Stanislav Abramov <abramov.center@yandex.ru>
2016-05-03 15:09:28 +0000

    library.h edited online with Bitbucket

--- library.h
+++ library.h
@@ -3,16 +3,17 @@
 
 typedef void* graph_user;
 
-graph_user reader_graph_edges(FILE* file);
-graph_user reader_graph_matrix(FILE* file);
+graph_user reader_graph_edges(char* text);
+graph_user reader_graph_matrix(char* text);
 int existence(graph_user temp_graf, int first_node, int second_node);
 void delete_graph(graph_user graph);
+void delete_edge(graph_user temp_graf, int first_node, int second_node);
 int func_count_edges(graph_user temp_graf);
 int func_count_node(graph_user temp_graf);
 graph_user make_graph(int form, int size);
 void add_edge_graph(graph_user temp_graf, int first_node, int second_node, int orient, int weight);
 int character(graph_user temp_graf, char* string, int first_node, int second_node);
 graph_user copy_graph(graph_user temp_graf);
-void load_graph_in_file(char* file, graph_user temp_graf);
+void load_graph_in_file(char* text, graph_user temp_graf);
 
 #endif
