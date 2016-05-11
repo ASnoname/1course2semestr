eb7bd7a1fe10f9567817ecab5d7280d25dcc2d36
Stanislav Abramov <abramov.center@yandex.ru>
2016-05-11 17:14:51 +0000

    30%

--- task_four.c
+++ task_four.c
@@ -1,104 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int part1(char** lamborghini, int high, int weight, int x, int y, int count){
+void part1(FILE* file, int** key_coo, int* weight, int* high){
 
-	if (lamborghini[x-1][y] == '*'){ 
-		if (lamborghini[weight-2][y] == ' ')
-			lamborghini[weight-2][y] = '0' + count;
-		else if (lamborghini[weight-2][y] == 'B')
-			return 1;
-	}	
-	else if (lamborghini[x-1][y] == ' ')
-		lamborghini[x-1][y] = '0' + count;	
-	else if (lamborghini[x-1][y] == 'B')
-		return 1;	
-
-	if (lamborghini[x+1][y] == '*'){
-		if (lamborghini[1][y] == ' ')
-			lamborghini[1][y] = '0' + count;
-		else if (lamborghini[1][y] == 'B')
-			return 1;		
-	}	
-	else if (lamborghini[x+1][y] == ' ')
-		lamborghini[x+1][y] = '0' + count;	
-	else if (lamborghini[x+1][y] == 'B')
-		return 1;	
-
-	if (lamborghini[x][y-1] == '*'){
-		if (lamborghini[x][high-2] == ' ')
-			lamborghini[x][high-2] = '0' + count;
-		else if (lamborghini[x][high-2] == 'B')
-			return 1;		
-	}	
-	else if (lamborghini[x][y-1] == ' ')
-		lamborghini[x][high-2] = '0' + count;	
-	else if (lamborghini[x][y-1] == 'B')
-		return 1;	
-
-	if (lamborghini[x][y+1] == '*'){
-		if (lamborghini[x][1] == ' ')
-			lamborghini[x][1] = '0' + count;
-		else if (lamborghini[x][1] == 'B')
-			return 1;		
-	}	
-	else if (lamborghini[x][y+1] == ' ')
-		lamborghini[x][1] = '0' + count;
-	else if (lamborghini[x][y+1] == 'B')
-		return 1;	
-
-	return 0;
-}
-
-int part2(char** lamborghini, int high, int weight, int x, int y){
-
-	int i,j;
-	int count = 1;    int p,q;
-
-	int result = part1(lamborghini,high,weight,x,y,count);
-
-	if (result == 0){
-
-		for (j = 1; j < high-1; j++)
-		{
-			for (i = 1; i < weight-1; i++){
-
-				if (lamborghini[i][j] == '0' + count)
-				{
-					x = i;
-					y = j;  
-					count++;
-					if (part1(lamborghini,high,weight,x,y,count) == 1)
-						return 1;
-
-					i = 1; j = 1;
-				}
-			}
-		}
-
-		return 0;
-	}	
-	else return 1;
-}
-
-void main(int argc, char* argv[])
-{
-	FILE* file; 
-	file = fopen(argv[1], "rb");
-
-    int high = 0;
-    int weight = 0;
+    int temp = 0; 
     char c;
-    int key_coo[2][2];
-    int temp = 0;
 
 	while(1){
 		c = fgetc(file);			
 
 		if(c == 13){
 
-			weight = temp + 1;
-			high++;
+			*weight = temp + 1;
+			(*high)++;
 			c = fgetc(file);
 			temp = -1;
 		}
@@ -106,23 +20,22 @@ void main(int argc, char* argv[])
 
 		if (c == 'A'){
 			key_coo[0][0] = temp; 
-			key_coo[0][1] = high; 	
+			key_coo[0][1] = *high; 	
 		}	
 		if (c == 'B'){
 			key_coo[1][0] = temp; 
-			key_coo[1][1] = high; 	
+			key_coo[1][1] = *high; 	
 		}
 
 		if (c == EOF) break;
 	}
+}
 
-	int i,j;
-	char** lamborghini = calloc(weight,sizeof(char*));
-
-	for (i = 0; i < weight; i++)
-		lamborghini[i] = calloc(high,sizeof(char));
+void part2(FILE* file, int** lamborghini, int weight, int high){
 
 	fseek(file,0,SEEK_SET);
+	int i,j;
+	char c;
 
 	for (j = 0; j < high; j++)
 	{
@@ -138,13 +51,292 @@ void main(int argc, char* argv[])
 			lamborghini[i][j] = c;
 		}
 	}
+}
 
-	int x = key_coo[0][0];
-	int y = key_coo[0][1];	
+void correct(int** lamborghini, int weight, int high){
 
-	int verification = part2(lamborghini,high,weight,x,y);
+	int i,j;
 
-	// printf("%d\n", verification);
+	for (i = 1; i < high-1; i++){
+		for (j = 1; j < weight-1; j++){
+			printf("%d", lamborghini[j][i]);
+		}
+		printf("\n");
+	}	
+}
+
+void part3(int** lamborghini, int** copylabirint, int** key_coo, int weight, int high){
+
+	int i,j;
+
+	for (i = 0; i < high; i++){
+		for (j = 0; j < weight; j++){
+
+			if (lamborghini[j][i] == 32)
+				copylabirint[j][i] = 0;
+			else
+				copylabirint[j][i] = lamborghini[j][i];
+		}
+	}
+	
+	copylabirint[key_coo[0][0]][key_coo[0][1]] = 0;	
+	copylabirint[key_coo[1][0]][key_coo[1][1]] = 0;	
+}
+
+void part4(int** copylabirint, int** key_coo, int weight, int high){
+
+	int i,j;
+
+	for (i = 1; i < high-1; i++)
+	{
+		for (j = 1; j < weight-1; j++)
+		{
+			if (copylabirint[j][i] != 35){
+				if ((copylabirint[j+1][i] == 42) && (copylabirint[1][i] != 35)) 
+						copylabirint[j][i] += 1;
+
+				else if ((copylabirint[j+1][i] != 35) && (copylabirint[j+1][i] != 42))
+					copylabirint[j][i] += 1;
+
+				if ((copylabirint[j-1][i] == 42) && (copylabirint[weight-2][i] != 35))
+						copylabirint[j][i] += 1;
+
+				else if ((copylabirint[j-1][i] != 35) && (copylabirint[j-1][i] != 42))
+					copylabirint[j][i] += 1;
+
+				if ((copylabirint[j][i+1] == 42) && (copylabirint[j][1] != 35))
+						copylabirint[j][i] += 1;
+
+				else if ((copylabirint[j][i+1] != 35) && (copylabirint[j][i+1] != 42))
+					copylabirint[j][i] += 1;	
+
+				if ((copylabirint[j][i-1] == 42) && (copylabirint[j][high-2] != 35))
+						copylabirint[j][i] += 1;
+
+				else if ((copylabirint[j][i-1] != 35) && (copylabirint[j][i-1] != 42))
+					copylabirint[j][i] += 1;					
+			}
+		}
+	}
+}
+
+void part5(int** lamborghini, int** key_coo, int weight, int high){
+
+	int i,j;
+
+	for (i = 0; i < high; i++)
+		for (j = 0; j < weight; j++)
+			if (lamborghini[j][i] == 32)
+				lamborghini[j][i] = 0;
+
+	lamborghini[key_coo[0][0]][key_coo[0][1]] = 0;	
+	lamborghini[key_coo[1][0]][key_coo[1][1]] = 0;			
+}
+
+void part6_1(int** lamborghini, int** key_coo, int* x, int* y, int* count, int* optimal_way){ // вправо
+
+	lamborghini[*x][*y] = 1; 	
+	(*x)++;
+
+	while (lamborghini[*x][*y] != 35){  
+
+		if (lamborghini[*x][*y] == 42)
+			*x = 1;
+
+		if (lamborghini[*x][*y] == 1)
+			break;
+
+		if ((*count > lamborghini[*x][*y]) && (lamborghini[*x][*y] != 0))
+			break;
+
+		lamborghini[*x][*y] = ++(*count);
+
+		if (*optimal_way > 0)
+			if(lamborghini[*x][*y] > *optimal_way)
+				break;	
+
+		if ((*x == key_coo[1][0]) && (*y == key_coo[1][1])) 
+			return;
+
+		++(*x);	
+	}
+}
+
+void part6_2(int** lamborghini, int** key_coo, int* x, int* y, int* count, int* optimal_way){
+
+	(*y)++;
+
+	while (lamborghini[*x][*y] != 35){  
+
+		if (lamborghini[*x][*y] == 42)
+			*y = 1;
+
+		if (lamborghini[*x][*y] == 1)
+			break;
+
+		if ((*count > lamborghini[*x][*y]) && (lamborghini[*x][*y] != 0))
+			break;
+
+		lamborghini[*x][*y] = ++(*count);
+
+		if (*optimal_way > 0)
+			if(lamborghini[*x][*y] > *optimal_way)
+				break;	
+
+		if ((*x == key_coo[1][0]) && (*y == key_coo[1][1])) 
+			return;
+
+		++(*y);	
+	}	// вниз
+}
+
+void part6_3(int** lamborghini, int** key_coo, int* x, int* y, int* count, int* optimal_way, int weight){
+
+	--(*x);
+
+	while (lamborghini[*x][*y] != 35){  
+
+		if (lamborghini[*x][*y] == 42)
+			*x = weight-2;
+
+		if (lamborghini[*x][*y] == 1)
+			break;
+
+		if ((*count > lamborghini[*x][*y]) && (lamborghini[*x][*y] != 0))
+			break;
+
+		lamborghini[*x][*y] = ++(*count);
+
+		if (*optimal_way > 0)
+			if(lamborghini[*x][*y] > *optimal_way)
+				break;	
+
+		if ((*x == key_coo[1][0]) && (*y == key_coo[1][1])) 
+			return;
+
+		--(*x);	
+	} // влево
+}
+
+void part6_4(int** lamborghini, int** key_coo, int* x, int* y, int* count, int* optimal_way, int high){
+
+	(*y)--;
+
+	while (lamborghini[*x][*y] != 35){  
+
+		if (lamborghini[*x][*y] == 42)
+			*y = high-2;
+
+		if (lamborghini[*x][*y] == 1)
+			break;
+
+		if ((*count > lamborghini[*x][*y]) && (lamborghini[*x][*y] != 0))
+			break;
+
+		lamborghini[*x][*y] = ++(*count);
+
+		if (*optimal_way > 0)
+			if(lamborghini[*x][*y] > *optimal_way)
+				break;	
+
+		if ((*x == key_coo[1][0]) && (*y == key_coo[1][1])) 
+			return;
+
+		--(*y);	
+	}	 // вверх
+}
+
+void part6(int** lamborghini, int** copylabirint, int** key_coo, int weight, int high){
+
+	int i,j;
+	int x; int y;
+	x = key_coo[0][0];
+	y = key_coo[0][1];
+
+	int optimal_way = lamborghini[key_coo[1][0]][key_coo[1][1]];
+	int count = 1;
+
+	part6_1(lamborghini,key_coo,&x,&y,&count,&optimal_way);
+
+	if (lamborghini[x][y] == 35) 
+		x--;
+	else if (x == 1)
+		x = weight-2;
+
+	if ((x == key_coo[1][0]) && (y == key_coo[1][1]))
+		return;
+
+	part6_2(lamborghini,key_coo,&x,&y,&count,&optimal_way);
+
+	if (lamborghini[x][y] == 35)
+		y--;
+	else if (y == 1)
+		y = high-2;
+
+	if ((x == key_coo[1][0]) && (y == key_coo[1][1]))
+		return;
+
+	part6_3(lamborghini,key_coo,&x,&y,&count,&optimal_way,weight);
+
+	if (lamborghini[x][y] == 35)
+		x++;
+	else if (x == weight-2)
+		x = 1;
+
+	if ((x == key_coo[1][0]) && (y == key_coo[1][1]))
+		return;	
+
+	part6_4(lamborghini,key_coo,&x,&y,&count,&optimal_way,high);
+
+	if (lamborghini[x][y] == 35)
+		y = high-2;
+	else if (x == weight-2)
+		y++;
+
+	if ((x == key_coo[1][0]) && (y == key_coo[1][1]))
+		return;		
+
+}
+
+void main(int argc, char* argv[])
+{
+	FILE* file; 
+	file = fopen(argv[1], "rb");
+
+	int i,j;
+
+    int** key_coo = calloc(2,sizeof(int*));
+	for (i = 0; i < 2; i++)
+		key_coo[i] = calloc(2,sizeof(int));
+
+    int high = 0;
+    int weight = 0;
+
+    part1(file, key_coo, &weight, &high);
+
+	int** lamborghini = calloc(weight,sizeof(int*));
+
+	for (i = 0; i < weight; i++)
+		lamborghini[i] = calloc(high,sizeof(int));
+
+	part2(file,lamborghini,weight,high);
+
+	int** copylabirint = calloc(weight,sizeof(int*));
+	for (i = 0; i < weight; i++)
+		copylabirint[i] = calloc(high,sizeof(int));
+
+	part3(lamborghini, copylabirint, key_coo, weight, high);
+
+	part4(copylabirint, key_coo, weight, high);
+
+	part5(lamborghini, key_coo, weight,high);
+
+	part6(lamborghini,copylabirint,key_coo,weight,high);
+
+	
+
+	correct(lamborghini, weight, high);
+
 
 
 }
